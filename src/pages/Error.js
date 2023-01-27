af3024cd35fc7ff51fcd56476f2ef83c56a74d7a
Motiur Muhith <1motiur4@gmail.com>
2023-01-26 22:48:09 -0500

    commit 2

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <section className='section'>
-      <h2>Error</h2>
+      <h2>404 error!!</h2>
+      <p>page not found womp womp</p>
+      <Link to="/">Go back home</Link>
     </section>
   );
 };
